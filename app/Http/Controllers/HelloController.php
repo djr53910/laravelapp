bc5e67e03da4a3fede2609dd3f38bfbfe698424b
toriyami <continuation9389@gmail.com>
2018-05-07 21:47:13 +0900

    practice laravel view composer

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -8,13 +8,8 @@ use Illuminate\Http\Response;
 class HelloController extends Controller {
 
 public function index() {
-    $data = [
-        ['name' => '山田たろう', 'mail' => 'taro@yamda'],
-        ['name' => '田中はなこ', 'mail' => 'hanako@flower'],
-        ['name' => '鈴木さちこ', 'mail' => 'sachico@happy']
-    ];
 
-    return view('hello.index',['data'=>$data]);
+    return view('hello.index', ['message' => 'Hello']);
   }
 
 }
